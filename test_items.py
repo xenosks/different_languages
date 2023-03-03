a6781fc08177362aeb041392675b99dc9d00fa5a
Ksenia <xenosks@gmail.com>
2023-03-03 10:26:30 +0500

    Add file

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 
 class TestMainPage1():
-    def test_language(self, browser):
+    def test_check_button_add_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
